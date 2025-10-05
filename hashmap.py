c90635e937a4a41ba3528a038c2ddfc4fbe44d3d
geancarlos <geangbr@gmail.com>
2025-10-05 17:22:32 -0300

    Two Sum

--- hashmap.py
+++ hashmap.py
@@ -4,6 +4,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hasher = {}
         for idx, i in enumerate(nums):
-            if hasher.get(i) is None:
-                return [hasher.get(i), idx]
+            complement = target - i
+            if complement in hasher:
+                return [hasher[complement], idx]
             hasher[i] = idx
